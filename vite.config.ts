6796753f6552f03b60f9ee2fecabe6d876b58de5
Val-Priad <valeriipriadchenko@gmail.com>
2025-09-05 14:23:13 +0200

    fixed vite.config.ts again

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kpi-4-lab-1-ui-ux/",
+  base: "/kpi-4-lab-1-ui-ux",
 });
