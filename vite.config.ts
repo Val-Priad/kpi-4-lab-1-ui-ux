67edd21b64945570efcb6003397fb98716781ee0
Val-Priad <valeriipriadchenko@gmail.com>
2025-09-05 14:13:22 +0200

    changed strategy and fixed all issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "kpi-4-lab-1-ui-ux",
+});
